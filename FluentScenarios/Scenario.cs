ecad7f1be10796a978be90b43ae5752f28b881bb
Mats Jalas <mats.jalas@gmail.com>
2019-11-09 13:09:42 +0200

    Fix multi column table printing in test result.

--- FluentScenarios/Scenario.cs
+++ FluentScenarios/Scenario.cs
@@ -18,44 +18,9 @@ namespace FluentScenarios
             Steps.Add(new Step<StepAction> {Name = name, Action = new SimpleAction(action)});
         }
 
-        private static string GenerateTableString(Table table)
-        {
-            var output = new List<string>();
-            var firstRow = true;
-            var separatorLine = "";
-            foreach (var row in table.Rows)
-            {
-                switch (row)
-                {
-                    case IDictionary<string, object> rowAsDictionary:
-                    {
-                        var rowAsString = "|";
-                        if (firstRow)
-                        {
-                            rowAsString = rowAsDictionary.Aggregate(rowAsString, (current, entry) => 
-                                $"{current}\t{entry.Key}\t|");
-                            rowAsString += "\n";
-                            var separator = new String('-', rowAsString.Length+2);
-                            separatorLine = $"+{separator}+";
-                            rowAsString += $"{separatorLine}\n|";
-                            
-                            output.Add(separatorLine);
-                            firstRow = false;
-                        }
-                        rowAsString = rowAsDictionary.Aggregate(rowAsString, (current, entry) =>
-                            $"{current}\t{entry.Value}\t|");
-                        output.Add(rowAsString);
-                        break;
-                    }
-                }
-            }
-            output.Add(separatorLine);
-            return string.Join("\n", output);
-        }
-
         private void AddStep(string name, Table table, Action<Table> action)
         {
-            var tableString = GenerateTableString(table);
+            var tableString = TableStringBuilder.Build(table);
             var formattedStatement = $"{name} @resultMarker\n{tableString}";
             Statements.Add(name, formattedStatement);
             Steps.Add(new Step<StepAction>{Name = name, Action = new TableAction(action, table)});
